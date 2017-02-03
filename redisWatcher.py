8fee0fb10156ff934c5f9a472ecc1c375aca211d
dominik <me@domiz.cz>
2017-02-03 13:39:49 +0100

    Head and body extraction, validation

--- redisWatcher.py
+++ redisWatcher.py
@@ -8,6 +8,8 @@ class tag:
     warn = "[ \033[1;31mWARN\033[0m ] "
     dump = "[ "+colored("DUMP","cyan")+" ] "
 
+def validate_dsc(command):
+    return (True if command.startswith('#') and command.endswith(';') and command[1].isupper() and command[2].isupper() and command[3].isupper() else False)
 
 print tag.ok + "Process started"
 r = redis.StrictRedis(host='localhost',port=6379, db=0)
@@ -21,3 +23,9 @@ while(True):
     message = p.get_message()
     if(message):
         print tag.dump + "Received new message: " + str(message['data'])
+        print tag.info + "Valid DSC?: " + str(validate_dsc(str(message['data'])))
+        if(validate_dsc(str(message['data']))):
+            head = message['data'][1:3]
+            print tag.dump + "Extracted head: " + head
+            body = message['data'][4:-1]
+            print tag.dump + "Extracted body: " + body
