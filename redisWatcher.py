bcf25da11206c724108d9d8182b6af20366ad65d
dominik <me@domiz.cz>
2017-02-03 14:50:45 +0100

    Logging method implemented

--- redisWatcher.py
+++ redisWatcher.py
@@ -1,6 +1,6 @@
 import redis
 from termcolor import colored
-
+from time import gmtime, strftime
 
 class tag:
     ok = "[  " + colored("OK", "green") + "  ] "
@@ -25,36 +25,44 @@ def SVW(dsc):
     return True
 
 
+def log(level, message):
+    print getattr(tag,level) + message
+    filename = "redis-log-" + strftime("%Y-%m-%d", gmtime())
+    file = open(filename,'a')
+    file.write(strftime("%Y-%m-%d %H:%M:%S", gmtime()) +" "+ level + " >> " + message + "\r\n")
+
+
 def validate_dsc(command):
     return (
     True if command.startswith('#') and command.endswith(';') and command[1].isupper() and command[2].isupper() and
             command[3].isupper() else False)
 
 
-print tag.ok + "Process started"
+log("ok","Process started")
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 p = r.pubsub()
 try:
     p.subscribe('test')
-    print tag.ok + "Subscribing active"
+    log("ok","Subscription active")
 except:
-    print tag.fail + "Subscribing failed"
+    log("fail","Subscription failed")
+message = p.get_message()
 while (True):
     message = p.get_message()
     if (message):
-        print tag.dump + "Received new message: " + str(message['data'])
+        log("dump","Received new message: " + str(message['data']))
         if (validate_dsc(str(message['data']))):
             head = message['data'][1:4]
-            print tag.dump + "Extracted head: " + head
+            log("dump","Extracted head: " + head)
             body = message['data'][4:-1]
-            print tag.dump + "Extracted body: " + body
+            log("dump","Extracted body: " + body)
             possibles = globals().copy()
             possibles.update(locals())
             method = possibles.get(head)
             if not method:
-                print tag.fail + "Method %s is not implemented" % head
+                log("fail","Method %s is not implemented" % head)
             else:
                 method(body)
-                print tag.ok + "Done processing command " + message['data']
+                log("ok","Done processing command " + message['data'])
         else:
-            print tag.warn + "Message is not valid DSC command"
+            log("warn","Message is not valid DSC command")
