85e3a9944424fc99267f2d96fcd79e0b568c238b
Dominik Kadera <me@domiz.cz>
2017-02-03 12:46:46 +0100

    Working Redis

---
+++ redisWatcher.py
@@ -0,0 +1,9 @@
+import redis
+
+r = redis.StrictRedis(host='localhost',port=6379, db=0)
+p = r.pubsub()
+p.subscribe('test')
+while(True):
+    message = p.get_message()
+    if(message):
+        print "Received new message:" + message['data']
