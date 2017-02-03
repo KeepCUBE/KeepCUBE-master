19f7fc2338127d332c933d4f414423ed550990ea
dominik <me@domiz.cz>
2017-02-03 14:54:05 +0100

    Reformated code

--- redisWatcher.py
+++ redisWatcher.py
@@ -2,6 +2,7 @@ import redis
 from termcolor import colored
 from time import gmtime, strftime
 
+
 class tag:
     ok = "[  " + colored("OK", "green") + "  ] "
     info = "[ " + colored("INFO", "yellow") + " ] "
@@ -26,43 +27,43 @@ def SVW(dsc):
 
 
 def log(level, message):
-    print getattr(tag,level) + message
+    print getattr(tag, level) + message
     filename = "redis-log-" + strftime("%Y-%m-%d", gmtime())
-    file = open(filename,'a')
-    file.write(strftime("%Y-%m-%d %H:%M:%S", gmtime()) +" "+ level + " >> " + message + "\r\n")
+    file = open(filename, 'a')
+    file.write(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " " + level + " >> " + message + "\r\n")
 
 
 def validate_dsc(command):
     return (
-    True if command.startswith('#') and command.endswith(';') and command[1].isupper() and command[2].isupper() and
-            command[3].isupper() else False)
+        True if command.startswith('#') and command.endswith(';') and command[1].isupper() and command[2].isupper() and
+                command[3].isupper() else False)
 
 
-log("ok","Process started")
+log("ok", "Process started")
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 p = r.pubsub()
 try:
     p.subscribe('test')
-    log("ok","Subscription active")
+    log("ok", "Subscription active")
 except:
-    log("fail","Subscription failed")
+    log("fail", "Subscription failed")
 message = p.get_message()
 while (True):
     message = p.get_message()
     if (message):
-        log("dump","Received new message: " + str(message['data']))
+        log("dump", "Received new message: " + str(message['data']))
         if (validate_dsc(str(message['data']))):
             head = message['data'][1:4]
-            log("dump","Extracted head: " + head)
+            log("dump", "Extracted head: " + head)
             body = message['data'][4:-1]
-            log("dump","Extracted body: " + body)
+            log("dump", "Extracted body: " + body)
             possibles = globals().copy()
             possibles.update(locals())
             method = possibles.get(head)
             if not method:
-                log("fail","Method %s is not implemented" % head)
+                log("fail", "Method %s is not implemented" % head)
             else:
                 method(body)
-                log("ok","Done processing command " + message['data'])
+                log("ok", "Done processing command " + message['data'])
         else:
-            log("warn","Message is not valid DSC command")
+            log("warn", "Message is not valid DSC command")
