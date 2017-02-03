26d68d6e5bd3a7ca4f11273c8dd8143e12498d57
dominik <me@domiz.cz>
2017-02-03 12:52:52 +0100

    Added tags

--- redisWatcher.py
+++ redisWatcher.py
@@ -1,9 +1,23 @@
 import redis
+from termcolor import colored
 
+class tag:
+    ok = "[  "+colored("OK","green")+"  ] "
+    info = "[ "+colored("INFO","yellow")+" ] "
+    fail = "[ "+colored("FAIL","red")+" ] "
+    warn = "[ \033[1;31mWARN\033[0m ] "
+    dump = "[ "+colored("DUMP","cyan")+" ] "
+
+
+print tag.ok + "Process started"
 r = redis.StrictRedis(host='localhost',port=6379, db=0)
 p = r.pubsub()
-p.subscribe('test')
+try:
+    p.subscribe('test')
+    print tag.ok + "Subscribing active"
+except:
+    print tag.fail + "Subscribing failed"
 while(True):
     message = p.get_message()
     if(message):
-        print "Received new message:" + message['data']
+        print tag.dump + "Received new message: " + str(message['data'])
