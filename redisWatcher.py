5c1a2a9f60e7b42d44d4061563af61b0a3827672
dominik <me@domiz.cz>
2017-02-03 13:52:40 +0100

    Prepared functions for sending

--- redisWatcher.py
+++ redisWatcher.py
@@ -1,31 +1,60 @@
 import redis
 from termcolor import colored
 
+
 class tag:
-    ok = "[  "+colored("OK","green")+"  ] "
-    info = "[ "+colored("INFO","yellow")+" ] "
-    fail = "[ "+colored("FAIL","red")+" ] "
+    ok = "[  " + colored("OK", "green") + "  ] "
+    info = "[ " + colored("INFO", "yellow") + " ] "
+    fail = "[ " + colored("FAIL", "red") + " ] "
     warn = "[ \033[1;31mWARN\033[0m ] "
-    dump = "[ "+colored("DUMP","cyan")+" ] "
+    dump = "[ " + colored("DUMP", "cyan") + " ] "
+
+
+def SVA(dsc):
+    print tag.info + "Sending command " + dsc + " via ATMega"
+    return True
+
+
+def SVB(dsc):
+    print tag.info + "Sending command " + dsc + " via BlueTooth"
+    return True
+
+
+def SVW(dsc):
+    print tag.info + "Sending command " + dsc + " via WiFi"
+    return True
+
 
 def validate_dsc(command):
-    return (True if command.startswith('#') and command.endswith(';') and command[1].isupper() and command[2].isupper() and command[3].isupper() else False)
+    return (
+    True if command.startswith('#') and command.endswith(';') and command[1].isupper() and command[2].isupper() and
+            command[3].isupper() else False)
+
 
 print tag.ok + "Process started"
-r = redis.StrictRedis(host='localhost',port=6379, db=0)
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
 p = r.pubsub()
 try:
     p.subscribe('test')
     print tag.ok + "Subscribing active"
 except:
     print tag.fail + "Subscribing failed"
-while(True):
+while (True):
     message = p.get_message()
-    if(message):
+    if (message):
         print tag.dump + "Received new message: " + str(message['data'])
-        print tag.info + "Valid DSC?: " + str(validate_dsc(str(message['data'])))
-        if(validate_dsc(str(message['data']))):
+        if (validate_dsc(str(message['data']))):
             head = message['data'][1:3]
             print tag.dump + "Extracted head: " + head
             body = message['data'][4:-1]
             print tag.dump + "Extracted body: " + body
+            if head == 'SVA':
+                SVA(body)
+            elif head == 'SVB':
+                SVB(body)
+            elif head == 'SVW':
+                SVW(body)
+            else:
+                print tag.warn + "DSC was not recognized or is not supported"
+        else:
+            print tag.warn + "Message is not valid DSC command"
