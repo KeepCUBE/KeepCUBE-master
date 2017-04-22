0dee6301b6a383b6f53cb10a5f5e60904238735f
Dominik Kadera <me@domiz.cz>
2017-04-22 17:26:21 +0200

    serialWatcher

---
+++ serialWatcher.py
@@ -0,0 +1,60 @@
+import redis
+import serial
+from dscParser import parse_dsc
+from termcolor import colored
+from time import gmtime, strftime
+
+
+class tag:
+    ok = "[  " + colored("OK", "green") + "  ] "
+    info = "[ " + colored("INFO", "yellow") + " ] "
+    fail = "[ " + colored("FAIL", "red") + " ] "
+    warn = "[ \033[1;31mWARN\033[0m ] "
+    dump = "[ " + colored("DUMP", "cyan") + " ] "
+
+def STR(dsc):
+    print tag.info + "Sending command " + dsc + " via REDIS"
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    r.publish("atmr",dsc)
+    return True
+
+def log(level, message):
+    print getattr(tag, level) + message
+    filename = "redis-log-" + strftime("%Y-%m-%d", gmtime()) + ".log"
+    file = open(filename, 'a')
+    file.write(strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " " + level + " | " + message + "\r\n")
+
+
+def validate_dsc(command):
+    return (
+        True if command.startswith('#') and command.endswith(';') and command[1].isupper() and command[2].isupper() and
+                command[3].isupper() else False)
+
+
+log("ok", "Process started")
+ser = serial.Serial("/dev/ttyAMA0", 9600, timeout = 1)
+if(ser.isOpen() == False):
+    ser.open()
+log("ok", "Serial opened.")
+while (True):
+    message = ser.readline()
+    if (message):
+        log("dump", "Received new message: " + str(message['data']))
+        dsc = parse_dsc(message['data'])
+        if(dsc != False):
+            possibles = globals().copy()
+            possibles.update(locals())
+            method = possibles.get(dsc['head'])
+            if not method:
+                log("fail", "Method %s is not implemented" % dsc['head'])
+            else:
+                if False in dsc['body']:
+                    log("fail", "No parameters found!")
+                else:
+                    if 'C' not in dsc['body']:
+                        log("fail", "Command parameter not found!")
+                    else:
+                        method(dsc['body']['C'])
+                        log("ok", "Done processing command " + message['data'])
+        else:
+            log("fail", "Message was not valid DSC command")
